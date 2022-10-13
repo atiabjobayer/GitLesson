67d0af483d244cb964472c3623f3f108c0fcb61d
Atiab Jobayer <purnoak2013@gmail.com>
2022-10-13 23:44:56 +0530

    Initial Commit

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define MAX 100
+#define pb(x) push_back(x)
+#define pii pair< int, int >
+#define read(x) freopen(x, "r", stdin)
+#define write(x) freopen(x, "w", stdout)
+
+using namespace std;
+typedef long long int lli;
+
+int main() {
+    // read("in.txt"); write("out.txt");
+
+    cout << "Hello world" << endl;
+
+    return 0;
+}
